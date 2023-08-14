0e4076197e7722997910922c174a9fc3f4bec455
HeatSeaker <heatseaker321@gmail.com>
2023-08-14 16:54:28 +0500

    New Terrain added to the main scene

---
+++ Finale di Kart/Assets/_Scripts/Track/Track.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace _Scripts.Track {
+    public class Track : MonoBehaviour {
+        #region Variables
+
+        [SerializeField] private Transform endNode;
+
+        #endregion
+    }
+}
---
+++ Finale di Kart/Assets/_Scripts/Track/TrackGenerator.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Track {
+    public class TrackGenerator : MonoBehaviour{
+        #region Variables
+
+        [SerializeField] private List<Track> tracks;
+
+        #endregion
+    }
+}
